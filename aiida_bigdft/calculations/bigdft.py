ffb915ffed0d4c660ea50861a829a88baf86efe5
Augustin Degomme <test@test.com>
2020-11-30 16:43:42 +0100

    bigdft plugin : add support for yaml position in inputs and outputs.

--- aiida_bigdft/calculations/bigdft.py
+++ aiida_bigdft/calculations/bigdft.py
@@ -13,7 +13,7 @@ import os
 from aiida import orm
 from aiida.common import datastructures, exceptions
 from aiida.engine import CalcJob
-from aiida.orm.nodes.data import List, SinglefileData, Float
+from aiida.orm.nodes.data import List, SinglefileData, Float, Dict
 from aiida.plugins import DataFactory
 
 from BigDFT import Calculators as BigDFT_calc
@@ -22,7 +22,9 @@ from BigDFT import Inputfiles as BigDFT_files
 BigDFTParameters = DataFactory('bigdft')
 BigDFTLogfile = DataFactory('bigdft_logfile')
 
-#just override SystemCalculator constructor to avoid checking BIGDFT_ROOT variable
+# just override SystemCalculator constructor to avoid checking BIGDFT_ROOT variable
+
+
 class PluginSystemCalculator(BigDFT_calc.SystemCalculator):
     def __init__(self,
                  omp=os.environ.get('OMP_NUM_THREADS', '1'),
@@ -31,8 +33,9 @@ class PluginSystemCalculator(BigDFT_calc.SystemCalculator):
         # Use the initialization from the Runner class (so all options inside
         # __global_options)
         BigDFT_calc.Runner.__init__(self, omp=str(omp), mpi_run=mpi_run,
-                        dry_run=dry_run, skip=skip, verbose=verbose)
+                                    dry_run=dry_run, skip=skip, verbose=verbose)
         self.command = ""
+
 
 class BigDFTCalculation(CalcJob):
     """
@@ -42,24 +45,36 @@ class BigDFTCalculation(CalcJob):
     _INPUT_FILE_NAME = 'input.yaml'
     _OUTPUT_FILE_NAME = 'log.yaml'
     _TIMING_FILE_NAME = 'time.yaml'
+
     @classmethod
     def define(cls, spec):
         """Define inputs and outputs of the calculation."""
         # yapf: disable
         super(BigDFTCalculation, cls).define(spec)
-        spec.input('metadata.options.resources', valid_type=dict, default={'num_machines': 1, 'num_mpiprocs_per_machine': 1})
-        spec.input('metadata.options.parser_name', valid_type=six.string_types, default='bigdft')
-        spec.input('metadata.options.output_filename', valid_type=six.string_types, default=cls._OUTPUT_FILE_NAME)
-        spec.input('metadata.options.jobname', valid_type=six.string_types, required=False)
-        spec.input('parameters', valid_type=BigDFTParameters, help='Command line parameters for BigDFT')
-        spec.input('structure', valid_type=orm.StructureData, help='StructureData struct')
-        spec.input('structurefile', valid_type=orm.Str, help='xyz file', default=lambda: orm.Str(cls._POSINP_FILE_NAME))
-        spec.input('pseudos', valid_type=List, help='', default=lambda: List(), required=False)
-        spec.input('kpoints', valid_type=Float, help='kpoint mesh or kpoint path', default=lambda: Float(-1.0), required=False)
-        spec.input('extra_retrieved_files', valid_type=List, help='', default=lambda: List())
-        spec.output('bigdft_logfile', valid_type=BigDFTLogfile, help='BigDFT log file as a dict')
-        spec.exit_code(100, 'ERROR_MISSING_OUTPUT_FILES', message='Calculation did not produce all expected output files.')
-
+        spec.input('metadata.options.resources', valid_type=dict, default={
+                   'num_machines': 1, 'num_mpiprocs_per_machine': 1})
+        spec.input('metadata.options.parser_name',
+                   valid_type=six.string_types, default='bigdft')
+        spec.input('metadata.options.output_filename',
+                   valid_type=six.string_types, default=cls._OUTPUT_FILE_NAME)
+        spec.input('metadata.options.jobname',
+                   valid_type=six.string_types, required=False)
+        spec.input('parameters', valid_type=BigDFTParameters,
+                   help='Command line parameters for BigDFT')
+        spec.input('structure', valid_type=orm.StructureData,
+                   help='StructureData struct')
+        spec.input('structurefile', valid_type=orm.Str, help='xyz file',
+                   default=lambda: orm.Str(cls._POSINP_FILE_NAME))
+        spec.input('pseudos', valid_type=List, help='',
+                   default=lambda: List(), required=False)
+        spec.input('kpoints', valid_type=Dict, help='kpoint mesh or kpoint path',
+                   default=lambda: Dict(dict={}), required=False)
+        spec.input('extra_retrieved_files', valid_type=List,
+                   help='', default=lambda: List())
+        spec.output('bigdft_logfile', valid_type=BigDFTLogfile,
+                    help='BigDFT log file as a dict')
+        spec.exit_code(100, 'ERROR_MISSING_OUTPUT_FILES',
+                       message='Calculation did not produce all expected output files.')
 
     def prepare_for_submission(self, folder):
         """
@@ -69,87 +84,95 @@ class BigDFTCalculation(CalcJob):
             the calculation.
         :return: `aiida.common.datastructures.CalcInfo` instance
         """
-        #check that either structure or structurefile are set
-        #if both are set, fail.
+        # check that either structure or structurefile are set
+        # if both are set, fail.
 #        if self.inputs.structurefile is None and self.inputs.structure is None:
 #            raise exceptions.InputValidationError("either structure or structurefile must be set")
 #        if self.inputs.structurefile is not None and self.inputs.structure is not None:
 #            raise exceptions.InputValidationError("Only one of structure or structurefile must be set")
 
-        #somehow aiida sends back unicode strings here
+        # somehow aiida sends back unicode strings here
         dico = BigDFT_files.Inputfile()
-        dico.update( self.inputs.parameters.dict)
+        dico.update(self.inputs.parameters.dict)
 
-        bigdft_calc=PluginSystemCalculator()
+        bigdft_calc = PluginSystemCalculator()
+        local_copy_list = []
+        # if the structure is not already inside input dict
+        if not dico["posinp"]:
+            posinp_filename = self.inputs.structurefile.value
+            if self.inputs.structure is not None:
+                print("writing input posinp file")
+                posinp_string = self.inputs.structure._prepare_xyz()[0]
+                if "jobname" not in self.inputs.metadata.options:
+                    posinp_filename = self._POSINP_FILE_NAME
+                else:
+                    posinp_filename = self.inputs.metadata.options.jobname + ".xyz"
+    #            bigdft_calc.update_global_options(units="angstroem")
+                posinp_file = open(posinp_filename, "wb")
 
-        posinp_filename=self.inputs.structurefile.value
-        if self.inputs.structure is not None:
-            print("writing input posinp file")
-            posinp_string = self.inputs.structure._prepare_xyz()[0]
-            if "jobname" not in self.inputs.metadata.options:
-              posinp_filename = self._POSINP_FILE_NAME
-            else:
-              posinp_filename = self.inputs.metadata.options.jobname+".xyz"
-#            bigdft_calc.update_global_options(units="angstroem")
-            posinp_file = open(posinp_filename,"wb") 
+                posinp_file.write(posinp_string)
+                posinp_file.close()
+            posinp_filedata = SinglefileData(
+                file=os.path.abspath(posinp_filename)).store()
 
-            posinp_file.write(posinp_string)
-            posinp_file.close()
-        posinp_filedata = SinglefileData(file=os.path.abspath(posinp_filename)).store()
+    #        BigDFT_input.set_atomic_positions(dico, posinp_filename)
+    #        bigdft_calc._run_options(posinp={})
 
-#        BigDFT_input.set_atomic_positions(dico, posinp_filename)
-#        bigdft_calc._run_options(posinp={})
+            local_copy_list = [
+                (posinp_filedata.uuid, posinp_filedata.filename, posinp_filedata.filename)]
 
-        local_copy_list=[(posinp_filedata.uuid, posinp_filedata.filename,posinp_filedata.filename)]
-        #setup pseudopotentials if needed
+        # setup pseudopotentials if needed
         if self.inputs.pseudos is not None:
             for filename in self.inputs.pseudos:
-                pseudo_filedata = SinglefileData(file=os.path.abspath(filename)).store()
-                local_copy_list.append((pseudo_filedata.uuid, pseudo_filedata.filename,filename))
+                pseudo_filedata = SinglefileData(
+                    file=os.path.abspath(filename)).store()
+                local_copy_list.append(
+                    (pseudo_filedata.uuid, pseudo_filedata.filename, filename))
 
+        if self.inputs.kpoints:
+            var = {}
+            var["kpt"] = self.inputs.kpoints.dict
+            dico.update(var)
 
-        if self.inputs.kpoints.value != -1.0:
-            dico.update({'kpt': {
-                            'method': 'auto',
-                            'kptrlen': self.inputs.kpoints.value
-                            }
-                        })
-
-        #generate yaml input file from dict and whatever
+        # generate yaml input file from dict and whatever
 
         if "jobname" in self.inputs.metadata.options:
-            bigdft_calc.update_global_options(name=self.inputs.metadata.options.jobname)
+            bigdft_calc.update_global_options(
+                name=self.inputs.metadata.options.jobname)
 
         bigdft_calc._run_options(input=dico)
         bigdft_calc.pre_processing()
         if "jobname" not in self.inputs.metadata.options:
             input_filename = self._INPUT_FILE_NAME
         else:
-            input_filename = self.inputs.metadata.options.jobname+".yaml"
-        input_filedata = SinglefileData(file=os.path.abspath(input_filename)).store()
-        local_copy_list.append((input_filedata.uuid, input_filedata.filename,input_filename))
+            input_filename = self.inputs.metadata.options.jobname + ".yaml"
+        input_filedata = SinglefileData(
+            file=os.path.abspath(input_filename)).store()
+        local_copy_list.append(
+            (input_filedata.uuid, input_filedata.filename, input_filename))
 
         codeinfo = datastructures.CodeInfo()
         codeinfo.code_uuid = self.inputs.code.uuid
         outfile = self.inputs.metadata.options.output_filename
         timefile = self._TIMING_FILE_NAME
         if "jobname" in self.inputs.metadata.options:
-            outfile = "log-"+self.inputs.metadata.options.jobname+".yaml"
-            timefile = "time-"+self.inputs.metadata.options.jobname+".yaml"
+            outfile = "log-" + self.inputs.metadata.options.jobname + ".yaml"
+            timefile = "time-" + self.inputs.metadata.options.jobname + ".yaml"
 
 #        codeinfo.stdout_name = outfile
         codeinfo.withmpi = self.inputs.metadata.options.withmpi
         if "jobname" in self.inputs.metadata.options:
-            codeinfo.cmdline_params = ["--name="+self.inputs.metadata.options.jobname]
+            codeinfo.cmdline_params = ["--name=" +
+                                       self.inputs.metadata.options.jobname]
         #local_copy_list = []
         # Prepare a `CalcInfo` to be returned to the engine
         calcinfo = datastructures.CalcInfo()
         calcinfo.codes_info = [codeinfo]
         calcinfo.local_copy_list = local_copy_list
-        calcinfo.retrieve_list = [outfile, 
+        calcinfo.retrieve_list = [outfile,
                                   timefile,
-                                  "forces*", 
-                                  "final*", 
+                                  "forces_posinp.yaml",
+                                  "final_posinp.yaml",
                                   ["./debug/bigdft-err*", ".", 2]]
         calcinfo.retrieve_list.extend(self.inputs.extra_retrieved_files)
         return calcinfo
--- aiida_bigdft/workflows/relax.py
+++ aiida_bigdft/workflows/relax.py
@@ -8,6 +8,8 @@ from aiida.engine import WorkChain, append_, ToContext
 from aiida.orm.nodes.data import List
 from aiida.plugins import WorkflowFactory, DataFactory
 from BigDFT import InputActions
+from futile import YamlIO
+
 import numpy
 BigDFTBaseWorkChain = WorkflowFactory('bigdft')
 BigDFTParameters = DataFactory('bigdft')
@@ -34,8 +36,8 @@ class BigDFTRelaxWorkChain(WorkChain):
         spec.input('relax.algo', valid_type=orm.Str,
                    default=lambda: orm.Str('FIRE'),
                    help='algorithm to use during relaxation')
-        spec.input('relax.threshold_forces', valid_type=orm.Float,
-                   required=False, help='energy cutoff value, in ev/Ang')
+        spec.input('relax.threshold_forces', valid_type=orm.Float, required=False,
+                   default=lambda: orm.Float(0.0), help='energy cutoff value, in ev/Ang')
         spec.input('relax.steps', valid_type=orm.Int, required=False,
                    default=lambda: orm.Int(50),
                    help='number of relaxation steps to perform.')
@@ -56,12 +58,13 @@ class BigDFTRelaxWorkChain(WorkChain):
         self.ctx.inputs = AttributeDict(
             self.exposed_inputs(BigDFTBaseWorkChain))
         inputdict = self.inputs.parameters.dict
-        InputActions.optimize_geometry(inputdict,
-                                       self.inputs.relax.algo.value,
-                                       self.inputs.relax.steps.value)
+        if self.inputs.relax.perform:
+            InputActions.optimize_geometry(inputdict,
+                                           self.inputs.relax.algo.value,
+                                           self.inputs.relax.steps.value)
         if self.inputs.relax.threshold_forces is not None:
             InputActions.dict_set(inputdict, 'geopt', 'forcemax',
-                                  self.inputs.relax.threshold_forces.value/0.52917721067121)
+                                  self.inputs.relax.threshold_forces.value / 0.52917721067121)
 
         self.ctx.inputs.parameters = BigDFTParameters(dict=inputdict)
 
@@ -70,9 +73,16 @@ class BigDFTRelaxWorkChain(WorkChain):
         if self.inputs.extra_retrieved_files is not None:
             extra_retrieved_files.set_list(
                 self.inputs.extra_retrieved_files.get_list())
-        extra_retrieved_files.extend([["./data*/*.xyz", ".", 2],
-                                     ["./data*/geopt.mon", ".", 2],
-                                     ["./final_*xyz", ".", 2]])
+        extension = "xyz"
+        posinp = self.inputs.parameters.dict.get('posinp')
+        if posinp is not None:
+            try:
+                extension = posinp['properties']['format']
+            except KeyError:
+                extension = "yaml"
+        extra_retrieved_files.extend([["./data*/*." + extension, ".", 2],
+                                      ["./data*/geopt.mon", ".", 2],
+                                      ["./final_*", ".", 2]])
         self.ctx.inputs.extra_retrieved_files = extra_retrieved_files
 
         node = self.submit(BigDFTBaseWorkChain, **self.ctx.inputs)
@@ -86,58 +96,113 @@ class BigDFTRelaxWorkChain(WorkChain):
                 'Relaxation failed with exit status {}'.
                 format(workchain.exit_status))
             return self.exit_codes.ERROR_FAILED_RELAX
-
-        # final_*xyz file not found, it did not finish.
-        # We can output last *xyz file in data folder, restart, or fail
-        outstruct = "final_posinp.xyz"
-        repo = workchain.outputs.retrieved._repository._get_base_folder()
-        if "jobname" in self.inputs.run_opts.get_dict()['options']:
-            outstruct = "final_" +\
-                self.inputs.run_opts.get_dict()['options']['jobname'] + ".xyz"
-        try:
-            sf = repo.get_abs_path(outstruct, check_existence=True)
-        except OSError:
-            # do we have posout files ?
-            subname = "data"
+        extension = "xyz"
+        posinp = self.inputs.parameters.dict.get('posinp')
+        if posinp is not None:
+            try:
+                extension = posinp['properties']['format']
+            except KeyError:
+                extension = "yaml"
+        if self.inputs.relax.perform:
+            outstruct = "final_posinp." + extension
+            repo = workchain.outputs.retrieved._repository._get_base_folder()
             if "jobname" in self.inputs.run_opts.get_dict()['options']:
-                subname = subname + "-" +\
-                    self.inputs.run_opts.get_dict()['options']['jobname']
+                outstruct = "final_" +\
+                    self.inputs.run_opts.get_dict(
+                    )['options']['jobname'] + ".xyz"
+            try:
+                sf = repo.get_abs_path(outstruct, check_existence=True)
+            except OSError:
+                # final_*xyz file not found, it did not finish.
+                # We can output last *xyz file in data folder, restart, or fail
+                # do we have posout files ?
+                subname = "data"
+                if "jobname" in self.inputs.run_opts.get_dict()['options']:
+                    subname = subname + "-" +\
+                        self.inputs.run_opts.get_dict()['options']['jobname']
 
-            data_folder = repo.get_subfolder(subname)
-            posout_list = data_folder.get_content_list(pattern="posout*")
-            if posout_list.empty():
-                # not even, we failed. Should have been caught before.
+                data_folder = repo.get_subfolder(subname)
+                posout_list = data_folder.get_content_list(pattern="posout*")
+                if posout_list.empty():
+                    # not even, we failed. Should have been caught before.
+                    self.report(
+                        'Relaxation failed - no output found')
+                    return self.exit_codes.ERROR_FAILED_RELAX
+                sf = repo.get_abs_path(posout_list.sort()[-1][0])
+        else:
+            # no relaxation performed, file is named forces_posinp.xyz .. or yaml
+
+            outstruct = "forces_posinp." + extension
+            repo = workchain.outputs.retrieved._repository._get_base_folder()
+            if "jobname" in self.inputs.run_opts.get_dict()['options']:
+                outstruct = "forces_" +\
+                    self.inputs.run_opts.get_dict(
+                    )['options']['jobname'] + ".xyz"
+            try:
+                sf = repo.get_abs_path(outstruct, check_existence=True)
+            except OSError:
                 self.report(
                     'Relaxation failed - no output found')
                 return self.exit_codes.ERROR_FAILED_RELAX
-            sf = repo.get_abs_path(posout_list.sort()[-1][0])
 
         s = StructureData()
         # BigDFT xyz files have more data on the first line which confuse aiida
         # Get rid of them (and get energy and forces in the process)
-        try:
-            with open(sf) as f:
-                content = f.readlines()
-        except FileNotFoundError:
+
+        if extension == "xyz":
+            try:
+                with open(sf) as f:
+                    content = f.readlines()
+            except FileNotFoundError:
+                self.report(
+                    'Relaxation failed - no output position file found')
+                return self.exit_codes.ERROR_FAILED_RELAX
+            firstline = content[0].split()
+            content[0] = firstline[0] + '\n'
+            self.out('total_energy', orm.Float(firstline[2]).store())
+
+            forces_index = content.index(" forces\n")
+            forces = content[forces_index + 1:]
+            for i, f in enumerate(forces):
+                forces[i] = f.split()[1:4]
+
+            array_forces = ArrayData()
+            array_forces.set_array('forces', numpy.array(forces))
+            self.out('forces', array_forces.store())
+
+            positions = content[0:forces_index]
+            s._parse_xyz("".join(positions))
+            s._adjust_default_cell(vacuum_addition=0.0,
+                                   pbc=self.inputs.structure.pbc)
+        elif extension == "yaml":
+            logfile = workchain.outputs.bigdft_logfile.logfile
+            if(isinstance(logfile, list)):
+                energy = logfile[-1].get('Energy (Hartree)')
+            else:
+                energy = logfile.get('Energy (Hartree)')
+            self.out('total_energy', orm.Float(energy).store())
+            if(isinstance(logfile, list)):
+                forces = logfile[-1].get('Atomic Forces (Ha/Bohr)')
+            else:
+                forces = logfile.get('Atomic Forces (Ha/Bohr)')
+            for i, f in enumerate(forces):
+                forces[i] = list(f.values())[0]
+            array_forces = ArrayData()
+            array_forces.set_array('forces', numpy.array(forces))
+            self.out('forces', array_forces.store())
+            logl = YamlIO.load(sf)
+            if logl is None:
+                self.report(
+                    'Relaxation failed - no output position file found')
+                return self.exit_codes.ERROR_FAILED_RELAX
+            log = logl[0]
+            s.cell = log['abc']
+            for i in log['positions']:
+                s.append_atom(symbols=list(i.keys())[
+                              0], position=list(i.values())[0])
+        else:
             self.report(
-                'Relaxation failed - no output position file found')
+                'Relaxation failed - no parsing available for output type ' + extension)
             return self.exit_codes.ERROR_FAILED_RELAX
-
-        firstline = content[0].split()
-        content[0] = firstline[0] + '\n'
-        self.out('total_energy', orm.Float(firstline[2]).store())
-
-        forces_index = content.index(" forces\n")
-        forces = content[forces_index + 1:]
-        for i, f in enumerate(forces):
-            forces[i] = f.split()[1:4]
-
-        array_forces = ArrayData()
-        array_forces.set_array('forces', numpy.array(forces))
-        self.out('forces', array_forces.store())
-
-        positions = content[0:forces_index]
-        s._parse_xyz("".join(positions))
-        s._adjust_default_cell(vacuum_addition=0.0, pbc=self.inputs.structure.pbc)
         self.out('relaxed_structure', s.store())
         self.out_many(self.exposed_outputs(workchain, BigDFTBaseWorkChain))
